ccc4deb604282eee418c97a6bf88c292841bcd4f
Sam Peck <fullstacksam@protonmail.com>
2023-07-20 15:00:59 -0400

    idk man

--- Client/src/MusicPlayer.js
+++ Client/src/MusicPlayer.js
@@ -1,10 +1,55 @@
 //useRef allows me to store a value that persits between componet renders
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import mp3File from './Songs/rock-lobster.mp3'; //webpack bundles files before running the app so I had no idea why normal paths weren't working
 //you need to explicitly import files 
 
+
+// ##look at the object import that location pass that in as the source of the media player
+// ## look at require when you pass the path into the media player so it 
+
+
+
+
 function MusicPlayer(){
     const audioRef = useRef(null);  
+    const [playlistSongs, setPlaylistSongs] = useState([]);
+    const [curretSongIndex, setCurrentSongIndex]= useState(0);
+
+    const [location, setLocation] = useState(null)
+
+    const fetchPlaylistSongs = () =>{
+        const playlistId = 1 // will change this to a variable later
+
+        fetch(`/playlists/${playlistId}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+
+            setPlaylistSongs(data.playlist_song);
+            setLocation(playlistSongs[0].song.location)
+
+        })
+        .catch((error) => {
+          console.error('Error fetching playlist songs:', error);
+        });
+    };
+
+    useEffect(() => {
+        fetchPlaylistSongs();
+        
+    }, []);
+
+    useEffect(() =>{
+        console.log('Inside useEffect');
+        console.log(playlistSongs)
+    }, [playlistSongs]);
+
+
 
 
     const playRandomPart = () =>{
@@ -20,13 +65,42 @@ function MusicPlayer(){
         }
     };
 
+
+    const handleNextSong = () => {
+        setCurrentSongIndex((prevIndex) => (prevIndex + 1) % playlistSongs.length);
+        setLocation(playlistSongs[curretSongIndex].song.location); // Auto-play the next song
+        console.log(`current location is ${location}`)
+      };
+
+      if (playlistSongs == undefined){
+        return(
+            <div>
+                <p>waiting for load</p>
+            </div>
+        )
+      }
+
     return(
         <div>
         <audio ref={audioRef} controls>
-        <source src={mp3File} type="audio/mpeg" /> 
+        <source  src={location} type="audio/mpeg" /> 
         HEYYY, buddy your browser don't support the html5 audio player ahhhhhhhh lol
         </audio>
         <button onClick={playRandomPart}> Play random part</button>
+        <button onClick={handleNextSong}> Play Next Song</button>
+
+
+
+        <h2>Playlist Songs:</h2>
+      {playlistSongs.length > 0 ? (
+        <ul>
+          {playlistSongs.map((songObject) => (
+            <li key={songObject.id}>{songObject.song.title}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading playlist songs...</p>
+      )}
 
         </div>
 
