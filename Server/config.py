c91a1871840d71629c37224b8a8ad641ba641c21
Sam Peck <fullstacksam@protonmail.com>
2023-07-20 10:37:23 -0400

    progress

--- Server/config.py
+++ Server/config.py
@@ -7,7 +7,13 @@ from sqlalchemy import MetaData
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+##todo change this later for a .env file
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+
 app.json.compact = False
+
+
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
